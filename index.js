17d3878aa222017fd126664a6b601d76f26873a8
Duane Grell <duanegrell@gmail.com>
2023-01-11 00:19:46 -0500

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+
+}
+saturdayFun();
+
+function mondayWork (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+function wrapAdjective (flair = "*"){
+    return function (main = "special"){
+        return `You are ${flair}${main}${flair}!`
+    }
+}
+wrapAdjective();
